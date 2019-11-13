2c4e5fa03624c3c0fa2f829a97f82393eab69d82
stephaniey2022 <56015168+stephaniey2022@users.noreply.github.com>
2019-11-13 00:17:24 -0600

    Update login.js

---
+++ Project 6/login.js
@@ -0,0 +1,8 @@
+function createAccount(){
+	if (document.getElementById("firstname").value == "Sally"){
+		window.location.pathname= '/homepage.html';
+	}
+	else {
+		window.location.pathname= '/homepage2.html';
+	}
+}
