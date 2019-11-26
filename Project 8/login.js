3f758aecc71175b9cb376b9a374037f61fd446d5
meganyaur <meganyaur2022@u.northwestern.edu>
2019-11-26 01:09:13 -0600

    Fixed some stuff

---
+++ Project 8/login.js
@@ -0,0 +1,8 @@
+function createAccount(){
+	if (document.getElementById("firstname").value == "Sally"){
+		window.location.href= 'homepage.html';
+	}
+	else {
+		window.location.href= 'homepage2.html';
+	}
+}
