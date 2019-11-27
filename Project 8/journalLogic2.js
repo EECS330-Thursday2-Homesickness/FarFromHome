eb3f9d4d2d8b73399dda2fa179f9f1b50bf11e23
stephaniey2022 <56015168+stephaniey2022@users.noreply.github.com>
2019-11-27 16:00:30 -0600

    Update journalLogic2.js

--- Project 8/journalLogic2.js
+++ Project 8/journalLogic2.js
@@ -1,33 +1,5 @@
-/*
-var date = "mm/dd/year";
-var title = "Title";
-var entry = "Entry goes here";
-
-function getDate() {
-	date = document.getElementById("date_today").value;	
-	document.getElementById("mydate").innerHTML = "CHANGE";
-}
-
-function getTitle() {
-	title = document.getElementById("textentry").value;
-	document.getElementById("mytitle").innerHTMl = "ChANGE";
-}
-
-function getEntry() {
-	entry = document.getElementById("textentry").value;
-	document.getElementById("myentry").innerHTML = "CHANGE";
-}
-
-document.getElementById("lalala").onclick = function () {
-	window.open("journalNewEntry.html");
-	if (document.getElementById("myentry").value = "VARIABLE") {
-		document.getElementById("myentry").innerHTML = "123";
-	}
-}
-
-*/
-
 "use strict";
+
 
 const init = function(e) {
 	let spn = document.querySelector("#changed-text");
@@ -37,3 +9,5 @@ const init = function(e) {
 };
 document.addEventListener('DOMContentLoaded', function() {
 init();});
+
+
