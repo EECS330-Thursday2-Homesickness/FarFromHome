0842ba736113896b736f0af4c647c7d4a601d72c
stephaniey2022 <56015168+stephaniey2022@users.noreply.github.com>
2019-11-27 16:00:23 -0600

    Update journalLogic1.js

--- Project 8/journalLogic1.js
+++ Project 8/journalLogic1.js
@@ -1,36 +1,7 @@
-/*
-var date = "mm/dd/year";
-var title = "Title";
-var entry = "Entry goes here";
-
-function getDate() {
-	date = document.getElementById("date_today").value;	
-	document.getElementById("mydate").innerHTML = "CHANGE";
-}
-
-function getTitle() {
-	title = document.getElementById("textentry").value;
-	document.getElementById("mytitle").innerHTMl = "ChANGE";
-}
-
-function getEntry() {
-	entry = document.getElementById("textentry").value;
-	document.getElementById("myentry").innerHTML = "CHANGE";
-}
-
-document.getElementById("lalala").onclick = function () {
-	window.open("journalNewEntry.html");
-	if (document.getElementById("myentry").value = "VARIABLE") {
-		document.getElementById("myentry").innerHTML = "123";
-	}
-}
-
-*/
-
 "use strict";
 
 const init = function(e) {
-	let btn = document.querySelector("#lalala");
+	let btn = document.querySelector("#savebutton");
 	let newstring = document.getElementById("textentry").value;
 	
 	btn.addEventListener('click', function() {
